d01e82ca9883c85f7f451b1dffc05db9793c9d6c
GeoffNN <geoffrey.negiar@polytechnique.edu>
2017-02-09 17:28:00 +0100

    Added graph building from frequency address book

---
+++ src/graph.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def build_graph(address_book):
+    adj_by_sender = {}
+    for sender, val in address_book.items():
+        adj_by_sender[sender] = pd.Series([el[1] for el in val], index=[el[0] for el in val], name=sender)
+    adjacency_mat = pd.DataFrame.from_dict(adj_by_sender).T.fillna(0)
+    adjacency_mat = adjacency_mat.T.dot(adjacency_mat)
+    return adjacency_mat
