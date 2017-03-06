8a8bee68f01676c8a354c0064ce6c3462eba985c
GeoffNN <geoffrey.negiar@polytechnique.edu>
2017-03-06 14:09:53 +0100

    Added no limit option for ranking (all neighbors ranked for a sender) Added validation and test file output in pickle (centroids_demo)

--- centroids_demo.py
+++ centroids_demo.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
 from src.centroid_recommendation import compute_recommendations
-from src.preprocess import body_dict_from_panda
+from src.postprocess import write_results_ranked
+from src.preprocess import body_dict_from_panda, get_email_ids_per_sender, get_conversation_ids, get_all_senders
+from src.scoring import get_train_val, compute_prediction_mad
 from src.tfidftools import get_tfidf
 
 path_to_data = 'data/'
+path_to_results = 'results/'
 
-n_recipients = 10
+n_recipients = 'max'
 
 ##########################
 # load some of the files #
@@ -24,14 +27,40 @@ test_info = pd.read_csv(
 
 # Compute useful structures
 
-train_bodies = body_dict_from_panda(training_info)
+train_info, train_ids_per_sender_val, val_info, val_ids_per_sender = get_train_val(training, training_info,
+                                                                                   0.95, disp=True)
+
+train_bodies = body_dict_from_panda(train_info)
+val_bodies = body_dict_from_panda(val_info)
+
 test_bodies = body_dict_from_panda(test_info)
 
-body_dict = {**train_bodies, **test_bodies}
+body_dict = {**train_bodies, **val_bodies, **test_bodies}
 
 print('Fitting tfidf, this will take some time...')
 tfidf, tfs, keys = get_tfidf(body_dict, 0.001, 0.10)
 print("Fitted.")
 
-print("Computing recommendations...")
-recommendations = compute_recommendations(n_recipients, training, training_info, test, test_info, tfidf)
+print("Computing recommendations for train/val...")
+
+conversation_ids_val = get_conversation_ids(train_ids_per_sender_val, training_info)
+senders = get_all_senders(training)
+recommendations_val = compute_recommendations(n_recipients, senders, training_info, conversation_ids_val, val_info,
+                                              val_ids_per_sender,
+                                              tfidf)
+print("Done!")
+
+print("Computing recommendations for train/test...")
+train_ids_per_sender = get_email_ids_per_sender(training)
+conversation_ids = get_conversation_ids(train_ids_per_sender, training_info)
+test_ids_per_sender = get_email_ids_per_sender(test)
+recommendations_test = compute_recommendations(n_recipients, senders, training_info, conversation_ids, test_info,
+                                               test_ids_per_sender,
+                                               tfidf)
+print("Done!")
+print("Computing score on validation set...")
+print("Score: {}".format(compute_prediction_mad(recommendations_val, val_info)))
+
+print("Writing file...")
+write_results_ranked(recommendations_val, path_to_results, "centroids_validation")
+write_results_ranked(recommendations_test, path_to_results, "centroids_test")
--- src/centroid_recommendation.py
+++ src/centroid_recommendation.py
@@ -1,36 +1,27 @@
 import pandas as pd
 import numpy as np
 
-from src.preprocess import get_email_ids_per_sender, get_all_senders, get_conversation_ids
+try:
+    from tqdm import tqdm
+except ImportError:
+    tqdm = lambda x: x
 from src.tfidftools import get_tfidf_vector, get_tfidf_vectors, sparse_norm
 
-gamma = 1
 
-
-def compute_recommendations(n_recipients, training, training_info, test, test_info, tfidf):
-    train_ids_per_sender = get_email_ids_per_sender(training)
-    test_ids_per_sender = get_email_ids_per_sender(test)
-    conversation_ids = get_conversation_ids(train_ids_per_sender, training_info)
+def compute_recommendations(n_recipients, senders, training_info, conversation_ids, test_info, test_ids_per_sender,
+                            tfidf):
+    """Return recommendation dict : {mid: [rec1, rec2...] }"""
     recommendations = {}
-    senders = get_all_senders(training)
     print("Computing for {} senders".format(len(senders)))
-    k = 0
-    for sender in senders:
-        k += 1
-        if k % 10 == 0:
-            print("{} of {} senders computed".format(k, len(senders)))
-        print("Computing neighbor centroids for {}".format(sender))
+    pbar = tqdm(senders)
+    for sender in pbar:
         centroids = get_recipient_centroids(sender, conversation_ids, training_info, tfidf)
         mids_to_process = test_ids_per_sender[sender]
+        if len(mids_to_process) < 1:
+            continue
         print("Processing {} emails for {}".format(len(mids_to_process), sender))
-
-        j = 0
         for mid in mids_to_process:
-            print(mid)
             recommendations[mid] = recommend_for_mid(n_recipients, mid, centroids, test_info, tfidf)
-            if j % 5 == 0:
-                print("{}/{} emails processed".format(j, len(mids_to_process)))
-            j += 1
 
     return recommendations
 
@@ -44,14 +35,24 @@ def get_recipient_centroids(sender, conversation_ids, training_info, tfidf):
     return centroids
 
 
+def get_recipients(sender, conversation_ids):
+    return list(conversation_ids[sender].keys())
+
+
 def recommend_for_mid(n_recipients, mid, centroids, test_info, tfidf_model):
     feat = get_tfidf_vector(mid, test_info, tfidf_model)
+    # print(feat)
     return get_closest(n_recipients, centroids, feat).index
 
 
 def get_closest(n, centroids_dict, from_feat):
-    dists = {recipient: similarity(from_feat, to_feat) for recipient, to_feat in centroids_dict.items()}
-    return pd.Series(dists).sort_values()[:n]
+    """n = int or 'max' - if 'max', return the whole ranking"""
+    sims = {}
+    for recipient, to_feat in centroids_dict.items():
+        sims[recipient] = similarity(from_feat, to_feat)
+    if n == 'max':
+        return pd.Series(sims).sort_values(ascending=False)
+    return pd.Series(sims).sort_values(ascending=False)[:n]
 
 
 def centroid(mail_feats_array):
@@ -59,14 +60,12 @@ def centroid(mail_feats_array):
     return mean
 
 
-def get_recipients(sender, conversation_ids):
-    return conversation_ids[sender].keys()
-
-
 # TODO: use time diff in similarity computation
 def similarity(f_mail1, f_mail2):
-    return np.dot(f_mail1, f_mail2.T) / (sparse_norm(f_mail1) * sparse_norm(f_mail2))
+    if (sparse_norm(f_mail1) * sparse_norm(f_mail2)) == 0:
+        return 0
+    return float(f_mail1.dot(f_mail2.T) / (sparse_norm(f_mail1) * sparse_norm(f_mail2)))
 
 
-def decay(time_diff):
+def decay(time_diff, gamma=1):
     return gamma ** time_diff
